67942977ca839961078f4fde9f9049bb5ac93257
Prem Sichanugrist <s@sikachu.com>
2010-05-11 16:25:24 +0700

    Make it center

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,7 @@ var interval;
 
 $(function(){
   if(remaining){
+    $('#remaining').css('margin-top', $(document).height() / 2 - 70);
     updateTimer();
     interval = setInterval('updateTimer()', 1000); 
     $('#show').animate({backgroundColor: '#702615'}, remaining * 1000);
@@ -15,5 +16,6 @@ function updateTimer(){
   } else {
     clearInterval(interval);
     $('#remaining').html("Your tea is ready!");
+    alert("Your tea is ready!");
   }
 }
